2205b486e316b12b4b7011076943345810dc85c4
sy <sy@ddabo.com>
2018-11-16 23:56:41 +0900

    add

--- clova/index.js
+++ clova/index.js
@@ -87,7 +87,7 @@ class CEKRequest {
         //     diceCount = 1
         //   }
         // }
-        cekResponse.setSimpleSpeechText("주문해주셔서 감사합니다 알바들이 곧 호빵을 가지고 올꺼에요 좋은하루 되세요.")
+        cekResponse.setSimpleSpeechText("주문해주셔서 감사합니다 알바들이 곧 호빵을 가지고올꺼에요 좋은하루 되세요.")
         
         break
       
